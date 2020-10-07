9f18d04dac11848cce762ed401eb84576b9eaf49
Asad Iqbal <asad.dsoft@gmail.com>
2020-10-07 03:12:03 -0500

    initail layout

---
+++ src/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = () => {
+
+    return (
+        <aside className="sidebar">Sidebar</aside>
+    )
+}
+
+export default Sidebar
